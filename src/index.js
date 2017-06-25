b9110da30429f35444614f482e0d3efa3531e45f
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2017-06-25 10:30:39 -0300

    Add simple component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-// create a new component
-// this component should produce some HTML
+import React from 'react'
+import ReactDOM from 'react-dom'
 
+const App = () => {
+  return <div>Hi!</div>
+}
 
-// take this component generated HTML and put it on the page (in the DOM)
+ReactDOM.render(<App/>, document.querySelector('.container'))
