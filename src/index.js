b3d18eeaa1b95dea7feb4ac87f816c917b15931c
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2017-06-25 18:40:51 -0300

    Implement wait search

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import YTSearch from 'youtube-api-search'
+import _ from 'lodash'
 
 import SearchBar from './components/search_bar'
 import VideoList from './components/video_list'
 import VideoDetail from './components/video_detail'
+
+const API_KEY = 'YOUR_YOUTUBE_API_KEY'
 
 class App extends React.Component {
   constructor(props) {
@@ -32,9 +35,10 @@ class App extends React.Component {
   }
 
   render() {
+    const videoSearch = _.debounce(term => { this.videoSearch(term) }, 300)
     return (
       <div>
-        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+        <SearchBar onSearchTermChange={term => videoSearch(term)} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           videos={this.state.videos}
