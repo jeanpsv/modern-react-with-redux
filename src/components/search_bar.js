c93881dd337553ab99e4cb1b62059d7f7b97255f
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2017-06-25 14:05:44 -0300

    Implement handler to search bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,29 @@
 import React from 'react'
 
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      term: ""
+    }
+  }
+
+  onInputChange(event) {
+    this.setState({
+      term: event.target.value
+    })
+  }
+
   render() {
-    return <input onChange={event => console.log(event.target.value)} />
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange={event => this.onInputChange(event)}
+        />
+      </div>
+    )
   }
 }
 
