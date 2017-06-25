3f3ae777f262ad5658a4e782e90050cd7831bf3c
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2017-06-25 14:26:19 -0300

    Implementing video player and playlist

--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import YTSearch from 'youtube-api-search'
 
-const App = () => {
-  return <div>Hi!</div>
+import SearchBar from './components/search_bar'
+import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      videos: []
+    }
+
+    YTSearch({
+      key: API_KEY,
+      term: 'surf'
+    }, (videos) => {
+      this.setState({ videos })
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos={this.state.videos}/>
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(<App/>, document.querySelector('.container'))
